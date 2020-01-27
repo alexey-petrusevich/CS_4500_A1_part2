b09b3af338dc2b848ec483a4e08dd13bfb55ca27
Aliaksei <apetruesvich1@gmail.com>
2020-01-26 21:48:59 -0500

    Changed array.h per issue request from users of our API

--- array.h
+++ array.h
@@ -1,3 +1,4 @@
+//lang::CwC
 #pragma once
 #include "object.h"
 #include <cstring>
@@ -7,108 +8,674 @@
  * @file array.h
  * @brief Implementation of Array class.
  * @author Aliaksei Petrusevich and Megha Rao
+ * @date January 26, 2020
  */
 
 /**
- * Represents an array
+ * Represents an array of Objects.
  */
-class Array : public Object{
+class Array : public Object {
+	public:
+		Object** array;
+		size_t size;
 
-	Object* array;
-	size_t size;
+
+		/**
+		 * Default constructor of this array.
+		 */
+		Array() : Object() {
+		}
 
 
-	Array(size_t size) : Object() {
-		array = new Object();
-		size = 0;
-	}
+		/**
+		 * Constructor of this array of Object.
+		 *
+		 * @param size the base size of this array
+		 */
+		Array(size_t size) : Object() {
+		}
 
-	/**
-	 * Function to calculate the number of elements 
-	 * 
-	 * @param array - The array passed to calc length of
-	 * @return the length 
-	*/ 
-	size_t len(Object* array) {
-	}
 
-	/**
-	 * Function that checks if two arrays are equal 
-	 * 
-	 * @param array - second array
-	 * @return bool - T or F
-	 */
-	bool equalArray(char** arr2) {
-	}
+		/**
+		 * Returns the number of elements in this array.
+		 * 
+		 * @return the number of elements of this array
+		*/ 
+		virtual size_t len() {
+		}
 
-	/**
-	 * Function to return the element at the given index 
-	 * 
-	 * @param i - index 
-	 * @return - the string in array index 
-	 */
-	char* elementAt(int i) {
-	}
 
-	/**
-	 * Function to return the first index where the element exists
-	 * 
-	 * @param element - the string to be searched for 
-	 * @return the index
-	 */
-	int indexOf(char* element) {
-	}
+		/**
+		 * Checks whether the given array is equal to this array.
+		 * 
+		 * @param arr2 the second array
+		 * @return true if this array is equal to the given array and false otherwise
+		 */
+		virtual bool equalArray(Object** arr2) {
+		}
 
-	/**
-	 * Function to replace the element at the index with another element 
-	 * 
-	 * @param i - index 
-	 * @param element - replacemenet element 
-	 * 
-	 */
-	void replace(int i, char* element){
-	}
 
-	/**
-	 * Function to insert an element at index i and shift all other elements to the right by increasing the array size by 1 
-	 * 
-	 * @param i - index 
-	 * @param element - new element in array
-	 */
-	void insert(int i, char* element) {
-	}
+		/**
+		 * Function to return the element at the given index.
+		 * 
+		 * @param i the index of the element
+		 * @return the element of the array at the given index 
+		 */
+		virtual Object* elementAt(size_t i) {
+		}
 
-	/**
-	 * Function to remove the element at index and shift remaining elements to the left, decreasing the array size by 1 
-	 * 
-	 * @param i - index
-	 */
-	void remove(int i) {
-	}
 
-	/**
-	 * Function to split the array into two different arrays and returns the first half based on index
-	 * Array = Array 1 + Array 2
-	 * @param i - index
-	 * @return Array 1 (the first one)
-	 */
-	char** split(int i) {
-	}
+		/**
+		 * Function to return the first index where the element exists.
+		 * 
+		 * @param element the object being search for
+		 * @return the index of the array or >size() if element not found
+		 */
+		virtual size_t indexOf(Object* element) {
+		}
 
-	/**
-	 * Function to check whether the array is empty or not 
-	 * 
-	 * @param arr - the array to be checked
-	 * @return bool - T or F 
-	 */
-	bool isEmpty(char** arr) {
-	}
 
-	/*
-	 * The default destructor for the Object class.
-	*/
-	virtual ~Array() {
-		delete this->array;		
-	}
+		/**
+		 * Function to replace the element at the index with another element.
+		 * 
+		 * @param i index of the element 
+		 * @param element the element the element at index i is being replaced with
+		 * 
+		 */
+		virtual void replace(size_t i, Object* element){
+		}
 
+
+		/**
+		 * Function to insert an element at index i and shift all other 
+		 * elements to the right by increasing the array size by 1.
+		 * 
+		 * @param i insertion index
+		 * @param element new element of the array at the given index
+		 */
+		virtual void insert(size_t i, Object* element) {
+		}
+
+
+		/**
+		 * Function to remove the element at index and shift remaining elements to the left,
+		 * decreasing the array size by 1.
+		 * 
+		 * @param i index of the element being removed
+		 * @return the object at the given index
+		 */
+		virtual Object* remove(size_t i) {
+		}
+
+
+		/**
+		 * Returns an element at the given index.
+		 *
+		 * @param index the index of the element
+		 * @return the element of the array at the given index
+		 */
+		virtual Object* get(size_t index) {
+		}
+
+
+		/**
+		 * Function to split the array into two different arrays and returns
+		 * the first half based on index Array = Array 1 + Array 2.
+		 *
+		 * @param i - index
+		 * @return Array 1 (the first one)
+		 */
+		virtual Object** split(size_t i) {
+		}
+
+
+		/**
+		 * Function to check whether the array is empty or not.
+		 * 
+		 * @return true if this array is empty and false otherwise
+		 */
+		virtual bool isEmpty() {
+		}
+
+
+		/**
+		 * The destructor of this array.
+		 */
+		virtual ~Array() {
+		}
+};
+
+
+/**
+ * Represents an array of integers.
+ */
+class IntArray : public Object {
+	public:
+		int** array;
+		size_t size;
+
+
+		/**
+		 * Default constructor of this array.
+		 */
+		IntArray() : Object() {
+		}
+
+
+		/**
+		 * Constructor of this array of integers.
+		 *
+		* @param size the base size of this array
+		*/
+		IntArray(size_t size) : Object() {
+		}
+
+
+		/**
+		 * Returns the number of elements in this array.
+		 * 
+		 * @return the number of elements of this array
+		*/ 
+		virtual size_t len() {
+		}
+
+
+		/**
+		 * Checks whether the given array is equal to this array.
+		 * 
+		 * @param arr2 the second array
+		 * @return true if this array is equal to the given array and false otherwise
+		 */
+		virtual bool equalArray(int** arr2) {
+		}
+
+
+		/**
+		 * Function to return the element at the given index.
+		 * 
+		 * @param i the index of the element
+		 * @return the element of the array at the given index 
+		 */
+		virtual int* elementAt(size_t i) {
+		}
+
+
+		/**
+		 * Function to return the first index where the element exists.
+		 * 
+		 * @param element the object being search for
+		 * @return the index of the array or >size() if element not found
+		 */
+		virtual size_t indexOf(int* element) {
+		}
+
+
+		/**
+		 * Function to replace the element at the index with another element.
+		 * 
+		 * @param i index of the element 
+		 * @param element the element the element at index i is being replaced with
+		 * 
+		 */
+		virtual void replace(size_t i, int* element){
+		}
+
+
+		/**
+		 * Function to insert an element at index i and shift all other 
+		 * elements to the right by increasing the array size by 1.
+		 * 
+		 * @param i insertion index
+		 * @param element new element of the array at the given index
+		 */
+		virtual void insert(size_t i, int* element) {
+		}
+
+
+		/**
+		 * Function to remove the element at index and shift remaining elements to the left,
+		 * decreasing the array size by 1.
+		 * 
+		 * @param i index of the element being removed
+		 * @return the element at the given index
+		 */
+		virtual int* remove(size_t i) {
+		}
+
+
+		/**
+		 * Returns an element at the given index.
+		 *
+		 * @param index the index of the element
+		 * @return the element of the array at the given index
+		 */
+		virtual int* get(size_t index) {
+		}
+
+
+		/**
+		 * Function to split the array into two different arrays and returns
+		 * the first half based on index Array = Array 1 + Array 2.
+		 *
+		 * @param i index
+		 * @return Array 1 (the first one)
+		 */
+		virtual int** split(size_t i) {
+		}
+
+
+		/**
+		 * Function to check whether the array is empty or not.
+		 * 
+		 * @return true if this array is empty and false otherwise
+		 */
+		virtual bool isEmpty() {
+		}
+
+
+		/**
+		 * The destructor of this array.
+		 */
+		virtual ~IntArray() {
+		}
+};
+
+
+/**
+ * Represents an array of booleans.
+ */
+class BoolArray : public Object {
+	public:
+		bool** array;
+		size_t size;
+		
+
+		/**
+		 * Default constructor of this array.
+		 */
+		BoolArray() : Object() {
+		}
+
+	
+		/**
+		 * Constructor of this array of booleans.
+		 *
+		* @param size the base size of this array
+		*/
+		BoolArray(size_t size) : Object() {
+		}
+
+
+		/**
+		 * Returns the number of elements in this array.
+		 * 
+		 * @return the number of elements of this array
+		*/ 
+		virtual size_t len() {
+		}
+
+
+		/**
+		 * Checks whether the given array is equal to this array.
+		 * 
+		 * @param arr2 the second array
+		 * @return true if this array is equal to the given array and false otherwise
+		 */
+		virtual bool equalArray(bool** arr2) {
+		}
+
+
+		/**
+		 * Function to return the element at the given index.
+		 * 
+		 * @param i the index of the element
+		 * @return the element of the array at the given index 
+		 */
+		virtual bool* elementAt(size_t i) {
+		}
+
+
+		/**
+		 * Function to return the first index where the element exists.
+		 * 
+		 * @param element the object being search for
+		 * @return the index of the array or >size() if element not found
+		 */
+		virtual size_t indexOf(bool* element) {
+		}
+
+
+		/**
+		 * Function to replace the element at the index with another element.
+		 * 
+		 * @param i index of the element 
+		 * @param element the element the element at index i is being replaced with
+		 * 
+		 */
+		virtual void replace(size_t i, bool* element){
+		}
+
+
+		/**
+		 * Function to insert an element at index i and shift all other 
+		 * elements to the right by increasing the array size by 1.
+		 * 
+		 * @param i insertion index
+		 * @param element new element of the array at the given index
+		 */
+		virtual void insert(size_t i, bool* element) {
+		}
+
+
+		/**
+		 * Function to remove the element at index and shift remaining elements to the left,
+		 * decreasing the array size by 1.
+		 * 
+		 * @param i index of the element being removed
+		 * @return the element at the given index
+		 */
+		virtual bool* remove(size_t i) {
+		}
+
+
+		/**
+		 * Returns an element at the given index.
+		 *
+		 * @param index the index of the element
+		 * @return the element of the array at the given index
+		 */
+		virtual bool* get(size_t index) {
+		}
+
+
+		/**
+		 * Function to split the array into two different arrays and returns
+		 * the first half based on index Array = Array 1 + Array 2.
+		 *
+		 * @param i index
+		 * @return Array 1 (the first one)
+		 */
+		virtual bool** split(size_t i) {
+		}
+
+
+		/**
+		 * Function to check whether the array is empty or not.
+		 * 
+		 * @return true if this array is empty and false otherwise
+		 */
+		virtual bool isEmpty() {
+		}
+
+
+		/**
+		 * Destructor for this array.
+		 */
+		~BoolArray() {
+		}
+};
+
+
+/**
+ * Represents an array of floats.
+ */
+class FloatArray : public Object {
+	public:
+		float** array;
+		size_t size;
+
+
+		/**
+		 * Default constructor of this array.
+		 */
+		FloatArray() : Object() {
+		}
+
+
+		/**
+		 * Constructor of this array of floats.
+		 *
+		* @param size the base size of this array
+		*/
+		FloatArray(size_t size) : Object() {
+		}
+
+
+		/**
+		 * Returns the number of elements in this array.
+		 * 
+		 * @return the number of elements of this array
+		*/ 
+		virtual size_t len() {
+		}
+
+
+		/**
+		 * Checks whether the given array is equal to this array.
+		 * 
+		 * @param arr2 the second array
+		 * @return true if this array is equal to the given array and false otherwise
+		 */
+		virtual bool equalArray(float** arr2) {
+		}
+
+
+		/**
+		 * Function to return the element at the given index.
+		 * 
+		 * @param i the index of the element
+		 * @return the element of the array at the given index 
+		 */
+		virtual float* elementAt(size_t i) {
+		}
+
+
+		/**
+		 * Function to return the first index where the element exists.
+		 * 
+		 * @param element the object being search for
+		 * @return the index of the array or >size() if element not found
+		 */
+		virtual size_t indexOf(float* element) {
+		}
+
+
+		/**
+		 * Function to replace the element at the index with another element.
+		 * 
+		 * @param i index of the element 
+		 * @param element the element the element at index i is being replaced with
+		 * 
+		 */
+		virtual void replace(size_t i, float* element){
+		}
+
+
+		/**
+		 * Function to insert an element at index i and shift all other 
+		 * elements to the right by increasing the array size by 1.
+		 * 
+		 * @param i insertion index
+		 * @param element new element of the array at the given index
+		 */
+		virtual void insert(size_t i, float* element) {
+		}
+
+
+		/**
+		 * Function to remove the element at index and shift remaining elements to the left,
+		 * decreasing the array size by 1.
+		 * 
+		 * @param i index of the element being removed
+		 * @return the element at the given index
+		 */
+		virtual float* remove(size_t i) {
+		}
+
+
+		/**
+		 * Returns an element at the given index.
+		 *
+		 * @param index the index of the element
+		 * @return the element of the array at the given index
+		 */
+		virtual float* get(size_t index) {
+		}
+
+
+		/**
+		 * Function to split the array into two different arrays and returns
+		 * the first half based on index Array = Array 1 + Array 2.
+		 *
+		 * @param i index
+		 * @return Array 1 (the first one)
+		 */
+		virtual float** split(size_t i) {
+		}
+
+
+		/**
+		 * Function to check whether the array is empty or not.
+		 * 
+		 * @return true if this array is empty and false otherwise
+		 */
+		virtual bool isEmpty() {
+		}
+
+		/**
+		 * Destructor for this array.
+		 */
+		~FloatArray() {
+		}
+};
+
+
+/**
+ * Represents an array of Strings.
+ */
+class StrArray : public Array {
+	public:
+		/**
+		 * Default constructor of this array.
+		 */
+		StrArray() : Array() {
+		}
+
+
+		/**
+		 * Constructor of this array of floats.
+		 *
+		* @param size the base size of this array
+		*/
+		StrArray(size_t size) : Array(size) {
+		}
+
+
+		/**
+		 * Returns the number of elements in this array.
+		 * 
+		 * @return the number of elements of this array
+		*/ 
+		virtual size_t len() {
+		}
+
+
+		/**
+		 * Checks whether the given array is equal to this array.
+		 * 
+		 * @param arr2 the second array
+		 * @return true if this array is equal to the given array and false otherwise
+		 */
+		virtual bool equalArray(String** arr2) {
+		}
+
+
+		/**
+		 * Function to return the element at the given index.
+		 * 
+		 * @param i the index of the element
+		 * @return the element of the array at the given index 
+		 */
+		virtual String* elementAt(size_t i) {
+		}
+
+
+		/**
+		 * Function to return the first index where the element exists.
+		 * 
+		 * @param element the object being search for
+		 * @return the index of the array or >size() if element not found
+		 */
+		virtual size_t indexOf(String* element) {
+		}
+
+
+		/**
+		 * Function to replace the element at the index with another element.
+		 * 
+		 * @param i index of the element 
+		 * @param element the element the element at index i is being replaced with
+		 * 
+		 */
+		virtual void replace(size_t i, String* element){
+		}
+
+
+		/**
+		 * Function to insert an element at index i and shift all other 
+		 * elements to the right by increasing the array size by 1.
+		 * 
+		 * @param i insertion index
+		 * @param element new element of the array at the given index
+		 */
+		virtual void insert(size_t i, String* element) {
+		}
+
+
+		/**
+		 * Function to remove the element at index and shift remaining elements to the left,
+		 * decreasing the array size by 1.
+		 * 
+		 * @param i index of the element being removed
+		 * @return the element at the given index
+		 */
+		virtual String* remove(size_t i) {
+		}
+
+
+		/**
+		 * Returns an element at the given index.
+		 *
+		 * @param index the index of the element
+		 * @return the element of the array at the given index
+		 */
+		virtual String* get(size_t index) {
+		}
+
+
+		/**
+		 * Function to split the array into two different arrays and returns
+		 * the first half based on index Array = Array 1 + Array 2.
+		 *
+		 * @param i index
+		 * @return Array 1 (the first one)
+		 */
+		virtual String** split(size_t i) {
+		}
+
+
+		/**
+		 * Function to check whether the array is empty or not.
+		 * 
+		 * @return true if this array is empty and false otherwise
+		 */
+		virtual bool isEmpty() {
+		}
+
+
+		/**
+		 * Destructor of this class.
+		 */
+		~StrArray() {
+		}
 };
